814ab89e02f51938cda998a106c051406ceaf521
David Haas <Dhaas3@mail.greenriver.edu>
2021-04-13 22:36:51 -0700

    Add mapping of Publisher to Book & vice versa

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -17,6 +17,9 @@ public class Book
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -29,6 +32,16 @@ public class Book
     {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Publisher getPublisher()
+    {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher)
+    {
+        this.publisher = publisher;
     }
 
     public Long getId()
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by dh on 4/12/21
@@ -21,6 +20,10 @@ public class Publisher
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher()
     {}
 
@@ -31,6 +34,16 @@ public class Publisher
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks()
+    {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books)
+    {
+        this.books = books;
     }
 
     public Long getId()
