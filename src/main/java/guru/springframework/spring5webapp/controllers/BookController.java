eb36d786bec4888eb8643b678c27174e8a08b235
David Haas <Dhaas3@mail.greenriver.edu>
2021-04-14 11:24:40 -0700

    Created an MVC controller

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dh on 4/14/21
+ */
+@Controller
+public class BookController
+{
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository)
+    {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model)
+    {
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
